8c074dafae95350251edd2fddde40956d0ea1a9c
Vitor Queiroz <vitor.queiroszti@gmail.com>
2020-06-03 13:52:33 -0300

    Second commit

--- server/src/controllers/ItemsControllers.ts
+++ server/src/controllers/ItemsControllers.ts
@@ -5,6 +5,7 @@ class ItemsController {
   async index(req: Request, res: Response) {
     const items = await knex('items').select('*');
 
+
     const serializedItems = items.map((item) => ({
       id: item.id,
       title: item.title,
